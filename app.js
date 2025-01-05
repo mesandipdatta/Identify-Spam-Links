d7df2e30b18771bbaed505ea25bf1c2b08ddd33d
Sandip Datta <sandip.ddb@gmail.com>
2025-01-05 16:35:49 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,18 @@
+const getRedirectedUrl = async (shortUrl) => {
+    try {
+      const response = await fetch(shortUrl, { method: "HEAD", redirect: "follow" });
+      console.log("Redirected URL:", response.url);
+      console.log("Redirected URL:", response);
+    } catch (error) {
+      console.error("Error:", error.message);
+    }
+  };
+  
+  // Get the shortened URL from command-line arguments
+  const shortUrl = process.argv[2]; // The first argument after the script name
+  if (!shortUrl) {
+    console.error("Please provide a shortened URL as an argument.");
+    process.exit(1);
+  }
+  
+  getRedirectedUrl(shortUrl);
